6858bf41a4c3f8817aefa4c4438ba1170f197dd9
Rizan Zaky <rizan149@gmail.com>
2017-05-23 17:18:04 +0530

    Data format Update 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,15 +7,15 @@ import { DataProvider } from '../../providers/data/data';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public plans: Plans[];
+  public columnedPlans: Plans[][];
 
   constructor(public navCtrl: NavController, private dataService: DataProvider) {
     this.getPlansFromLocal();
   }
 
   public getPlansFromLocal() {
-    this.dataService.getPlans().then(resPlans => {
-      this.plans = resPlans;
+    this.dataService.getPlans(4).then(resPlans => {
+      this.columnedPlans = resPlans;
     });
   }
 }
--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -3,20 +3,71 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataProvider {
+  // private plans: Plans[][] = [
+  //   [
+  //     {planId: 1, icon: 'md-close', name: 'Be a Man'},
+  //     {planId: 2, icon: 'md-time', name: 'Financial'},
+  //     {planId: 3, icon: 'md-close', name: 'Angular 2'}
+  //   ],
+  //   [
+  //     {planId: 4, icon: 'md-time', name: 'MCP Exams'}
+  //   ]
+  // ];
+
   private plans: Plans[] = [
     {planId: 1, icon: 'md-close', name: 'Be a Man'},
-    {planId: 1, icon: 'md-time', name: 'Financial'},
-    {planId: 1, icon: 'md-close', name: 'Angular 2'},
-    {planId: 1, icon: 'md-time', name: 'MCP Exams'}
-  ]
+    {planId: 2, icon: 'md-time', name: 'Financial'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 3, icon: 'md-close', name: 'Angular 2'},
+    {planId: 4, icon: 'md-time', name: 'MCP Exams'}
+  ];
 
   constructor() {
   }
 
-  getPlans(): Promise<Plans[]> {
-    return new Promise<Plans[]>(resolve => {
-      resolve(this.plans);
+  getPlans(columns): Promise<Plans[][]> {
+    return new Promise<Plans[][]>(resolve => {
+      resolve( this.getColumned(columns) );
+
+      console.log("plans: ", this.plans);
+      // resolve(this.plans);
     });
+  }
+
+  getColumned(columns): Plans[][] {
+    // console.log("Columns: ", columns)
+    let columnedPlans = [];
+
+    let noOfPlans: number = this.plans.length;
+    let noOfIters: number = 1;
+    let modulus: number = noOfPlans / columns;
+    let modulo: number = noOfPlans % columns;
+
+    for(var i=0; i < noOfPlans; i = i+columns) {
+      // let colLength: number = (noOfPlans - noOfIters * columns) > columns ? columns : (noOfPlans - noOfIters * columns);
+      let colLength: number = (noOfIters <= modulus) ? columns : modulo;
+      noOfIters++;
+      let rowPlans: Plans[] = [];
+
+      for(var j=0; j < colLength; j++) {
+        rowPlans.push(this.plans[i+j]);
+      }
+
+      columnedPlans.push(rowPlans);
+    }
+    
+    console.log("columnedPlans: ", columnedPlans);
+
+    return(columnedPlans);
+    // return([]);
   }
 }
 
